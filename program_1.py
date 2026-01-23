381fb6305e095a9700a938b307617141c73a141e
Samuel-Renneke <srenneke@students.unwsp.edu>
2026-01-23 09:14:25 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,8 @@
+#Modified Hello Part 1
+#Samuel Renneke, 1/23/2026
 def hello_world():
-    # Update the message below to say "Hello World", 
-    # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    
+    print('Hello World')
 
 hello_world()
+
